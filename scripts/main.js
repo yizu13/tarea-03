786aa7a91af82691b4598ce92001d0b3d9f9df7c
Jesús Hernández <jesushdez1303@gmail.com>
2025-05-08 23:49:55 -0400

    fuap

--- scripts/main.js
+++ scripts/main.js
@@ -20,34 +20,90 @@ functions.map((element)=>{
     newFunction.textContent = element.symbol;
     container.appendChild(newFunction);
 
+    document.getElementById("resultField").textContent = "Resultado";
+
+    const htmlNumber1 = document.getElementById("number1");
+    const htmlNumber2 = document.getElementById("number2");
+
     if(element.name !== "result" && element.name !== "delete" && element.name !== "clean"){
     newFunction.addEventListener("click", ()=>{
         const sign = document.getElementById("operation");
         sign.textContent = element.symbol;
+
     });
         } else if(element.name === "result"){
             newFunction.addEventListener("click", ()=>{
-                try{
-              const number1 = parseInt(document.getElementById("number1").value);
-              const number2 = parseInt(document.getElementById("number2").value);
-
+              const number1 = parseInt(htmlNumber1.value);
+              const number2 = parseInt(htmlNumber2.value);
+    
               const operation = document.getElementById("operation").textContent;
-              let realOperator;
-              let resultChanged;
-
-              if(operation === "%")
+              const result = document.getElementById("resultField");
               
               if(!isNaN(number1) && !isNaN(number2)){
-                resultChanged = eval(number1.toString() + operation + number2.toString());
-                console.log(`resultado: ${resultChanged}`);
-                document.getElementById("resultField").innerText = `${resultChanged}`;
-              }
-            }catch(err){
-                console.log("Debe ser un número", err);
-            }
-            });
-        }
-});
+                
+                switch(operation){
 
+                    case "+":
+                     result.textContent = number1 + number2;
+                    break;
+
+                    case "-":
+                        result.textContent = number1 - number2;
+                        break;
+
+                    case "x":
+                        result.textContent = number1 * number2;
+                        break;
+
+                    case "√": 
+                        if(number1 === 2 || number1 === 3){
+                        result.textContent = parseInt((number2 ** (1/number1)).toFixed(5));
+                    }
+                        else{
+                            console.log("error")
+                            result.textContent = "Solo raíces de 2 y 3"
+                        }
+                        break;
+
+                    case "^":
+                        result.textContent = parseInt((number1 ** number2).toFixed(5));
+                        break;
+                    
+                    case "÷":
+                        if(number2 !== 0){
+                        result.textContent = number1 / number2;
+                    }else{
+                        console.log("error")
+                        result.textContent = "error";
+                    }                        
+                        break;
+
+                    case "%":
+                        result.textContent = number1 % number2;
+                        break;
+
+                        default: 
+                        console.log("La operación no puede ser ejecutada");
+                        result.textContent = "error"
+
+                }
+              }
+                
+            
+            });
+        } else if(element.name === "clean"){
+            newFunction.addEventListener("click", ()=>{
+                document.getElementById("resultField").textContent = "Resultado";
+                htmlNumber1.value = "";
+                htmlNumber2.value = "";
+            });
+
+    } else if(element.name === "delete"){
+        newFunction.addEventListener("click", ()=>{
+            htmlNumber1.value = "";
+            htmlNumber2.value = "";
+        })
+    }
+});
 
 
